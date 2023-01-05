e128e7e867a4c5156f8d7b0e6cafc3a7e49b715e
megdlawit <megdalenea@gmail.com>
2023-01-05 22:18:55 +0300

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
-def add(a: float, b: float) -> float:
+'''
+    Basic annotations for variables.
+'''
 
+
+def add(a: float, b: float) -> float:
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
